ee2a42ea69df76f21b553a63e31ed226c494a8bd
SarossyBence <79597030+SarossyBence@users.noreply.github.com>
2021-04-19 11:13:35 +0200

    Map v1.2
    basic map logic

--- prog/JSlender/src/Map.java
+++ prog/JSlender/src/Map.java
@@ -18,21 +18,21 @@ class Map extends JPanel {
     private final Font LABEL_FONT = new Font(Font.DIALOG, Font.PLAIN, 40);
     public JLabel[][] grid = new JLabel[15][15];
     public int[][] gridhow = {
-            {1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {1, 1, 1, 0, 2, 2, 0, 0, 4, 4, 4, 4, 4, 4, 0},
-            {1, 1, 1, 0, 2, 2, 0, 0, 5, 5, 5, 5, 5, 4, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 5, 5, 5, 4, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 5, 5, 5, 4, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 5, 5, 5, 4, 0},
-            {1, 1, 1, 0, 0, 0, 0, 0, 4, 5, 5, 5, 5, 4, 0},
-            {1, 1, 1, 0, 0, 0, 0, 0, 4, 5, 5, 5, 5, 4, 0},
+            {1, 1, 1, 0, 0, 0, 3, 3, 3, 0, 2, 0, 0, 0, 0},
+            {1, 1, 1, 2, 0, 0, 3, 3, 3, 0, 0, 0, 2, 0, 0},
             {1, 1, 1, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 7, 7, 7, 0, 0, 5, 5, 4, 5, 5, 4, 0},
+            {2, 0, 0, 7, 7, 7, 0, 0, 4, 5, 5, 5, 5, 4, 0},
+            {0, 0, 0, 0, 0, 0, 0, 2, 4, 4, 4, 4, 5, 4, 2},
+            {1, 1, 1, 0, 0, 0, 0, 0, 4, 5, 4, 5, 5, 4, 0},
+            {1, 1, 1, 0, 0, 0, 3, 3, 4, 5, 5, 5, 5, 4, 0},
+            {1, 1, 1, 0, 0, 0, 3, 3, 4, 4, 4, 4, 5, 4, 0},
+            {0, 0, 2, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 2, 2},
+            {0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 2, 2},
+            {0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 3, 3, 0, 0, 0},
+            {0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 6, 6, 2, 0, 0},
+            {2, 2, 0, 0, 2, 2, 0, 0, 0, 0, 6, 6, 2, 0, 0},
+            {2, 2, 0, 0, 2, 2, 0, 0, 0, 0, 6, 6, 0, 0, 0},
     };
 
 
@@ -42,16 +42,16 @@ class Map extends JPanel {
         Panel.setBackground(Color.BLACK);
             for(int row = 0; row<grid.length;row++) {
                  for (int col = 0; col < grid[row].length; col++) {
-                     System.out.print(gridhow[row][col]);
+                     //System.out.print(gridhow[row][col]);
                      grid[row][col] = new JLabel("      ", SwingConstants.CENTER);
                      grid[row][col].setFont(LABEL_FONT); // make it big
                      grid[row][col].setOpaque(true);
                      if(gridhow[row][col]==0){
-                                   grid[row][col].setBackground(Color.WHITE);
-                                    Panel.add(grid[row][col]);}
-                      if (gridhow[row][col]==1){
-                             grid[row][col].setBackground(Color.gray);
-                              Panel.add(grid[row][col]);}
+                         grid[row][col].setBackground(Color.WHITE);
+                         Panel.add(grid[row][col]);}
+                     if (gridhow[row][col]==1){
+                         grid[row][col].setBackground(Color.gray);
+                         Panel.add(grid[row][col]);}
                      if (gridhow[row][col]==2){
                          grid[row][col].setBackground(Color.green);
                          Panel.add(grid[row][col]);}
@@ -59,10 +59,16 @@ class Map extends JPanel {
                          grid[row][col].setBackground(Color.blue);
                          Panel.add(grid[row][col]);}
                      if (gridhow[row][col]==4){
+                         grid[row][col].setBackground(Color.orange);
+                         Panel.add(grid[row][col]);}
+                     if (gridhow[row][col]==5){
                          grid[row][col].setBackground(Color.yellow);
                          Panel.add(grid[row][col]);}
-                     if (gridhow[row][col]==4){
-                         grid[row][col].setBackground(Color.orange);
+                     if (gridhow[row][col]==6){
+                         grid[row][col].setBackground(Color.cyan);
+                         Panel.add(grid[row][col]);}
+                     if (gridhow[row][col]==7){
+                         grid[row][col].setBackground(Color.black);
                          Panel.add(grid[row][col]);}
                  } }
             JPanel bottomPanel = new JPanel();
